2bdc82062e21aa79936b6cacf7d93ebebdba2f9e
Benjamin BONTEMPS <zigaroula@hotmail.fr>
2015-04-17 21:36:37 +0200

    Debut implémentation du calcul des mouvements du fou

--- piece.h
+++ piece.h
@@ -19,6 +19,13 @@ class Piece
         std::vector<int> getPosition() { return std::vector<int>(posX, posY); }
         ///Calcule l'ensemble des mouvements d'une pièce
         void computeAvailableMovements(std::vector<Piece>, std::vector<Piece>);
+        ///Fonctions séparées pour calculer les mouvements
+        void computePawn(std::vector<Piece>, std::vector<Piece>);
+        void computeRook(std::vector<Piece>, std::vector<Piece>);
+        void computeKnight(std::vector<Piece>, std::vector<Piece>);
+        void computeBishop(std::vector<Piece>, std::vector<Piece>);
+        void computeQueen(std::vector<Piece>, std::vector<Piece>);
+        void computeKing(std::vector<Piece>, std::vector<Piece>);
     private:
         int posX;
         int posY;
