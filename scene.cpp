287576006ce4ed16058eb14fa8449c3c2637d1f6
Vincent Timofti <vincent.timofti@gmail.com>
2015-04-17 14:26:24 +0200

    load obj with texture

--- scene.cpp
+++ scene.cpp
@@ -13,7 +13,7 @@ void Scene::initScene(int width, int height)
 {
     projection_matrix = glm::mat4(0.5f);
     //vao_list.push_back(Vao::getCube());
-    vao_list.push_back(Vao::loadObj("models/big.obj", glm::vec3(0.5f, 0.f, 0.f)));
+    vao_list.push_back(Vao::loadObj("models/pion.obj", glm::vec3(0.5f, 0.f, 0.f)));
     
     camera = Camera(width, height);
 }
